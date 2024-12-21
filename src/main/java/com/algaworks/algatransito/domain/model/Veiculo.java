59219679a4214a51366c4c767a30b52955c2e21f
LucasLopes777 <lucaslopesledok@gmail.com>
2024-12-21 12:28:56 -0300

    Módulo 5 concluído

--- src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
+++ src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
@@ -1,20 +1,10 @@
 package com.algaworks.algatransito.domain.model;
 
-import com.algaworks.algatransito.domain.validation.ValidationGroups;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import jakarta.persistence.*;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.groups.ConvertGroup;
-import jakarta.validation.groups.Default;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
 @Setter
